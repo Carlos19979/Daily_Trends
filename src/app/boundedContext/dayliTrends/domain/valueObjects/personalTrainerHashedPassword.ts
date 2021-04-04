eda4a29f85ca47686bb07a4370b19de72b7497a3
carlos <carloscode23@gmail.com>
2021-04-04 12:34:17 +0200

    deleted unecesary files

--- src/app/boundedContext/dayliTrends/domain/valueObjects/personalTrainerHashedPassword.ts
+++
@@ -1,9 +0,0 @@
-import { BaseString } from '../../../shared/domain/valueObjects/baseString';
-
-export class PersonalTrainerHashedPassword extends BaseString {
-
-  constructor(value: string) {
-    super(value);
-  }
-
-}
--- src/app/boundedContext/dayliTrends/domain/valueObjects/personalTrainerName.ts
+++
@@ -1,9 +0,0 @@
-import { BaseString } from '../../../shared/domain/valueObjects/baseString';
-
-export class PersonalTrainerName extends BaseString {
-
-  constructor(value: string) {
-    super(value);
-  }
-
-}
